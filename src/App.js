311a694db970383e810195e9ffb483fcb13bbd5e
Matvienko Denis <Denis.Matvienko@bcsoft.com>
2021-02-06 15:49:45 +0300

    Loader

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Loader from './Loader/Loader';
+import Table from './Table/Table';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    isLoading: true,
+    data: []
+  }
+
+  async componentDidMount() {
+    const response = await fetch(`http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D`)
+    const data = await response.json()
+
+    // Необходимо изменить state когда у нас загрузились данные
+    this.setState({
+      isLoading:false,
+      data
+    })
+  }
+
+  render() {
+    return(
+      <div className="container">
+        {
+          this.state.isLoading
+          ? <Loader /> // если (state isLoading) - не загрузилиь данные, работает Loader
+          : <Table 
+            data={this.state.data} /> // если данные загрузились Table, куда передаем параметры data={this.state.data}
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
+
---
+++ src/Table/Table.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default props => (
+    <table className="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>firstName</th>
+                <th>lastName </th>
+                <th>email</th>
+                <th>phone </th>
+            </tr>
+        </thead>
+        <tbody>
+            {props.data.map(item => (
+                <tr key={item.id + item.phone}>
+                    <td>{item.id}</td>
+                    <td>{item.firstName}</td>
+                    <td>{item.lastName}</td>
+                    <td>{item.email}</td>
+                    <td>{item.phone}</td>
+                </tr>
+            ))}
+        </tbody> 
+    </table>     
+)
